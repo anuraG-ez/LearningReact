ee3e9b5a19874dabc44241d9dfbfb3990b59212a
anuraG-ez <anuragnandi116@gmail.com>
2024-03-15 01:11:34 +0530

    initial commi

---
+++ script.js
@@ -0,0 +1,7 @@
+let heading = React.createElement(
+"h1",
+{style:{color:"red"}, id: "heading"},
+"Namaste World 2");
+let root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
